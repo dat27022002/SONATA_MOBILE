eb25e26f20c1bf15e5df0551b1e837dac9bb3533
dat27022002 <dat.nguyen27022002@hcmut.edu.vn>
2024-07-19 15:08:27 +0700

    feat: UI header, config generate file apk, add pakage icon

--- src/config/navigation.js
+++ src/config/navigation.js
@@ -1,5 +1,34 @@
 import Welcome from '../pages/Welcome/Welcome';
 import Login from '../pages/Login';
+import { Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const configHeaderTitle = {
+    title: 'HJ POS',
+    headerStyle: {
+        backgroundColor: '#444243',
+    },
+    headerTitleStyle: {
+        fontWeight: 'bold',
+        fontSize: 20,
+    },
+    headerTintColor: '#fff',
+    headerTitleAlign: 'center',
+};
+
+const customButtonGoToMenu = () => {
+    const navigation = useNavigation();
+    return (
+        <TouchableOpacity
+            onPress={() => {
+                navigation.goBack();
+            }}
+        >
+            <Icon name="navicon" size={20} color="#FFFFFF" />
+        </TouchableOpacity>
+    );
+};
 
 const configNavigation = [
     {
@@ -10,6 +39,10 @@ const configNavigation = [
     {
         name: 'Welcome',
         component: Welcome,
+        options: {
+            ...configHeaderTitle,
+            headerLeft: customButtonGoToMenu,
+        },
     },
 ];
 
