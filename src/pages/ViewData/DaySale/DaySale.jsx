510a8445936db5e62badd2d6e568ee8b5f0c2613
dat27022002 <dat.nguyen27022002@hcmut.edu.vn>
2024-09-06 10:10:58 +0700

    fix: call API and display data for dayly sale list

--- src/pages/ViewData/DaySale/DaySale.jsx
+++ src/pages/ViewData/DaySale/DaySale.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import { View } from 'react-native';
 import { useTranslation } from 'react-i18next';
 import { getFormatedDate } from 'react-native-modern-datepicker';
@@ -18,6 +17,7 @@ import {
     ViewContainer,
     Loading,
 } from '../../../components';
+import { getDailySales } from './DailySaleLogic';
 
 const listStore = ['hyojung'];
 
@@ -34,19 +34,20 @@ const DaySale = () => {
     const [endDate, setEndDate] = useState(todayFormat);
     const [store, setStore] = useState('hyojung');
 
-    const [dataTableDetail, setDataTableDetail] = useState([]);
-    const [dataTableDetailChart, setDataTableDetailChart] = useState([]);
+    const [dataForChart, setDataForChart] = useState([]);
+    const [dataForTable, setDataForTable] = useState([]);
+    const [thisDaySales, setThisDaySales] = useState({ revenue: 0, quantity: 0 });
     const [loading, setLoading] = useState(false);
 
     const headerTable = ['Date', 'Quantity', 'Guest', 'Customer price', 'Sales amount'];
     const rowsWidth = [1.2, -1, -1, -1, 1.5];
 
     const dataChart = {
-        labels: dataTableDetailChart.slice(1).map((item) => item.Date.substring(5)),
+        labels: dataForChart.slice(1).map((item) => item.Date.substring(5)),
         datasets: [
             {
-                data: dataTableDetailChart.slice(1).map((item) => item['Sales amount']),
-                colors: dataTableDetailChart.slice(1).map(() => () => GlobalStyle.thirdTextColor),
+                data: dataForChart.slice(1).map((item) => item['Sales amount']),
+                colors: dataForChart.slice(1).map(() => () => GlobalStyle.thirdTextColor),
             },
         ],
     };
@@ -55,59 +56,18 @@ const DaySale = () => {
         setStore(value);
     };
 
-    const formatNumber = (number) => {
-        return new Intl.NumberFormat('en-US').format(number);
-    };
-
     const handleSearch = () => {
         setLoading(true);
-        axios
-            .get(`https://sonata-gmfycwecdkcnafey.eastus-01.azurewebsites.net/api/My/${startDate}/${endDate}`)
-            .then((response) => {
+        getDailySales(startDate, endDate, store)
+            .then((result) => {
                 setLoading(false);
-                const data = response.data;
-                const result = data.reduce((acc, curr) => {
-                    const date = curr.day.split('T')[0]; // Lấy ngày mà không lấy phần thời gian
-                    if (!acc[date]) {
-                        acc[date] = { Date: date, Quantity: 0, Guest: 0, 'Customer price': 0, 'Sales amount': 0 };
-                    }
-                    acc[date].Quantity += 1;
-                    acc[date].Guest += 1;
-                    acc[date]['Sales amount'] += curr.saleAmount;
-                    return acc;
-                }, {});
-
-                const resultArray = Object.values(result);
-                // Sắp xếp mảng theo thứ tự ngày
-                resultArray.sort((a, b) => new Date(a.Date) - new Date(b.Date));
-
-                // Tính tổng cho tất cả các ngày
-                const total = resultArray.reduce(
-                    (acc, curr) => {
-                        acc.Quantity += curr.Quantity;
-                        acc.Guest += curr.Guest;
-                        acc['Customer price'] += curr['Customer price'];
-                        acc['Sales amount'] += curr['Sales amount'];
-                        return acc;
-                    },
-                    { Date: '', Quantity: 0, Guest: 0, 'Customer price': 0, 'Sales amount': 0 },
-                );
-                // Thêm hàng tổng vào đầu mảng kết quả
-                resultArray.unshift(total);
-
-                setDataTableDetailChart(resultArray);
-
-                const formatResultArray = resultArray.map((value) => ({
-                    ...value,
-                    'Sales amount': formatNumber(value['Sales amount']),
-                }));
-
-                setDataTableDetail(formatResultArray);
-
-                //console.log('hello', JSON.stringify(resultArray, null, 2));
+                setDataForChart(result.dataChart);
+                setDataForTable(result.dataTable);
+                setThisDaySales(result.thisDaySales);
             })
-            .catch((error) => {
-                console.error('Error fetching data:', error);
+            .catch((err) => {
+                setLoading(false);
+                console.log(err);
             });
     };
 
@@ -145,9 +105,9 @@ const DaySale = () => {
 
             <BtnSearch handleSearch={handleSearch} />
 
-            <ViewSaleCurrent title={'Day sales'} saleAmount={0} quantity={0} />
+            <ViewSaleCurrent title={'Day sales'} saleAmount={thisDaySales.revenue} quantity={thisDaySales.quantity} />
 
-            <TableDetail data={dataTableDetail} headerTable={headerTable} rowsWidth={rowsWidth} />
+            <TableDetail data={dataForTable} headerTable={headerTable} rowsWidth={rowsWidth} />
 
             {/* chart */}
             <BarChartCustom dataChart={dataChart} />
